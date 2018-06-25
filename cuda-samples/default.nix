39447d277638509de3a66349667e9380909a5b72
tim put <timput@gmail.com>
2018-06-25 13:51:44 -0600

    remove references to compute_20 for compatitibility with cuda9

--- cuda-samples/default.nix
+++ cuda-samples/default.nix
@@ -5,7 +5,7 @@ in rec {
   examplecuda = stdenv.mkDerivation rec {
     name = "example-cuda";
     src = ./.;
-    buildInputs = [ pkgs.cudatoolkit pkgs.linuxPackages.nvidia_x11 pkgs.makeWrapper ];
+    buildInputs = [ pkgs.cudatoolkit pkgs.linuxPackages_4_16.nvidia_x11 pkgs.makeWrapper ];
 
     preBuild = ''
       export CUDA_PATH="${pkgs.cudatoolkit}"
@@ -18,7 +18,7 @@ in rec {
       for file in `ls $out/bin`; do
       	  echo "$file";
 	  wrapProgram "$out/bin/$file" \
-	              --prefix LD_LIBRARY_PATH ":" "${pkgs.linuxPackages.nvidia_x11}/lib"
+	              --prefix LD_LIBRARY_PATH ":" "${pkgs.linuxPackages_4_16.nvidia_x11}/lib"
       done
     '';
 
